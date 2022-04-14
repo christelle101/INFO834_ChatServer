1822ff1f1770994dd117e476edd898ae5164638b
Yawoumihani Hassani <72503019+yawoumi@users.noreply.github.com>
2022-04-14 17:19:21 +0200

    Update DashboardPage.js

--- chachaserverfront/src/Pages/DashboardPage.js
+++ chachaserverfront/src/Pages/DashboardPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import axios from "axios";
 import { Link } from "react-router-dom";
+import makeToast from '../Toaster';
 
 const DashboardPage = (props) => {
   const [chatrooms, setChatrooms] = React.useState([]);
+  const nameRef = React.createRef();
+
   const getChatrooms = () => {
     axios
       .get("http://localhost:3000/chatroom", {
@@ -18,11 +21,34 @@ const DashboardPage = (props) => {
           setTimeout(getChatrooms, 3000);
       });
     };
+
+  const addChatroom = () => {
+    const name = nameRef.current.value;
+
+    axios.post("http://localhost:3000/chatroom", {
+      name,
+    })
+    .then((response) => {
+      makeToast("success", response.data.message);
+      getChatrooms();
+    })
+    .catch((err) => {
+      console.log(err);
+      if (
+        err &&
+        err.response &&
+        err.response.data &&
+        err.response.data.message
+      )
+      makeToast("error", err.response.data.message);
+    });
+  };
       
   React.useEffect(() => {
     getChatrooms();
       // eslint-disable-next-line
   }, []);
+
 
   return (
       <div className="card">
@@ -35,22 +61,24 @@ const DashboardPage = (props) => {
                     name="chatroomName" 
                     id="chatroomName" 
                     placeholder="ChatterBox"
+                    ref = {nameRef}
     
                   />
             </div> 
           </div>
-          <button> Créer un Chatroom </button>
+          <button onClick={addChatroom}> Créer un Chatroom </button>
           <div className="chatrooms">
             {chatrooms.map((chatroom) =>(
-              <div key = {chatroom._id} className="chatroom">
-                  <div> {chatroom.name} </div>
-                    <Link to={"/chatroom/" + chatroom._id}>
-                      <div className = "join"> Join </div>
-                    </Link>
-                  </div>
-                ))}
+              <div key={chatroom._id} className="chatroom">
+                <div> {chatroom.name} </div>
+                <Link to={"/chatroom/" + chatroom._id}>
+                  <div className="join"> Rejoindre </div>
+                </Link>
               </div>
+                ))}
           </div>
+
+        </div>
         );
 };
     
